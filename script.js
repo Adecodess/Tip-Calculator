b242cf8bb484e8bc7c53f2980fabc6e716333bc1
codernaija11 <ademosuu@gmail.com>
2020-10-28 10:40:47 +0000

    adding New Project

---
+++ script.js
@@ -0,0 +1,47 @@
+var bill = document.getElementById("billamt");
+var service = document.querySelector("select");
+var people = document.getElementById("peopleamt");
+var button = document.getElementById("calculate");
+var tip = document.getElementById("totalTip");
+var tip2 = document.getElementById("tip2")
+
+
+
+
+function afterClick() {
+    if (bill.value != "" && service.value != 0 && people.value != "") {
+        totalTip = ""
+        tip.textContent = totalTip
+
+        totalTip = bill.value * service.value / people.value
+
+        totalTip = Math.round(totalTip * 100) / 100;
+        totalTip = totalTip.toFixed(2);
+        tip2.textContent = "Tip amount"
+        tip.textContent = totalTip
+
+
+    }
+
+}
+
+
+
+
+
+
+button.addEventListener("click", afterClick)
+bill.addEventListener("keypress", function (e) {
+    if (e.keyCode < 46 || e.keyCode > 58) {
+        e.preventDefault();
+    }
+})
+people.addEventListener("keypress", function (e) {
+    if (event.keyCode === 13) { afterClick(); }
+    else if (e.keyCode < 46 || e.keyCode > 58) {
+        e.preventDefault();
+    }
+
+
+})
+
