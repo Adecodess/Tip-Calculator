03819fe38ca390a48494e7aa59a5204dc7cb2dbe
codernaija11 <ademosuu@gmail.com>
2020-11-04 17:57:49 +0100

    adding new file

--- script.js
+++ script.js
@@ -3,45 +3,32 @@ var service = document.querySelector("select");
 var people = document.getElementById("peopleamt");
 var button = document.getElementById("calculate");
 var tip = document.getElementById("totalTip");
-var tip2 = document.getElementById("tip2")
-
-
-
+var tip2 = document.getElementById("tip2");
 
 function afterClick() {
-    if (bill.value != "" && service.value != 0 && people.value != "") {
-        totalTip = ""
-        tip.textContent = totalTip
+  if (bill.value != "" && service.value != 0 && people.value != "") {
+    totalTip = "";
+    tip.textContent = totalTip;
 
-        totalTip = bill.value * service.value / people.value
+    totalTip = (bill.value * service.value) / people.value;
 
-        totalTip = Math.round(totalTip * 100) / 100;
-        totalTip = totalTip.toFixed(2);
-        tip2.textContent = "Tip amount"
-        tip.textContent = totalTip
-
-
-    }
-
+    totalTip = Math.round(totalTip * 100) / 100;
+    totalTip = totalTip.toFixed(2);
+    tip2.textContent = "Tip amount";
+    tip.textContent = "N " + totalTip;
+  }
 }
 
-
-
-
-
-
-button.addEventListener("click", afterClick)
+button.addEventListener("click", afterClick);
 bill.addEventListener("keypress", function (e) {
-    if (e.keyCode < 46 || e.keyCode > 58) {
-        e.preventDefault();
-    }
-})
+  if (e.keyCode < 46 || e.keyCode > 58) {
+    e.preventDefault();
+  }
+});
 people.addEventListener("keypress", function (e) {
-    if (event.keyCode === 13) { afterClick(); }
-    else if (e.keyCode < 46 || e.keyCode > 58) {
-        e.preventDefault();
-    }
-
-
-})
-
+  if (event.keyCode === 13) {
+    afterClick();
+  } else if (e.keyCode < 46 || e.keyCode > 58) {
+    e.preventDefault();
+  }
+});
